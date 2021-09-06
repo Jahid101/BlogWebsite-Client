40786773fa8f5d8a4c02078170d8169fa4beeb8e
Jahid101 <jahidhasananik.official@gmail.com>
2021-09-07 01:41:51 +0600

    done

--- src/Components/Dashboard/AddBlog/AddBlog.js
+++ src/Components/Dashboard/AddBlog/AddBlog.js
@@ -21,7 +21,7 @@ const AddBlog = () => {
         };
 
 
-        const url = `http://localhost:9999/addBlog`;
+        const url = `https://floating-savannah-09787.herokuapp.com/addBlog`;
         fetch(url, {
             method: 'POST',
             headers: {
